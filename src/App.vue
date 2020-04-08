9982296f041e0ced28a9ec288dd3095b5a34aff2
Teo Reat <teo.reat@gmail.com>
2020-04-08 15:56:45 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <button @click="show = !show">Some btn</button>
+        <transition name="fade">
+            <div class="some-div" v-if="show"></div>
+        </transition>
+        <transition name="slide" type="animation" appear>
+            <div class="some-div" v-if="show"></div>
+        </transition>
+        <transition
+                enter-active-class="animated bounce"
+                leave-active-class="animated shake">
+            <div class="some-div" v-if="show"></div>
+        </transition>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                show: true
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    .some-div {
+        width: 400px;
+        background: coral;
+        height: 40px;
+        margin: 10px auto;
+    }
+    /*animations*/
+    /*fade*/
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+    .fade-leave {
+
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    /*slide*/
+    .slide-enter {
+        opacity: 0;
+    }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+    .slide-leave {
+
+    }
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
+    }
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 </style>
